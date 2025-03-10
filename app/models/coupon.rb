d30003a5e885af2124b2966cb22bceb1ce355d66
Beverly Green <greenbev000@gmail.com>
2025-03-10 14:58:20 -0500

    modified class method for use count in coupon model, updated model tests

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,7 @@ class Coupon < ApplicationRecord
     belongs_to :merchant
     has_many :invoices
 
-    def use_count
-        invoices.count 
+    def self.use_count(coupon_id)
+        joins(:invoices).where(id: coupon_id).count
     end
 end
