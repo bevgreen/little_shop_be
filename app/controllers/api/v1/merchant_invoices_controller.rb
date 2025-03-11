79afaafff7c13488eed64263da1897d7898792a9
Beverly Green <greenbev000@gmail.com>
2025-03-11 10:21:42 -0500

    Added new route so coupon can be updated to an invoice, made sure coupon_id is in invoice serializer, working on update method in invoice controller

--- app/controllers/api/v1/merchant_invoices_controller.rb
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -15,4 +15,24 @@ class Api::V1::MerchantInvoicesController < ApplicationController
 
     render json: InvoiceSerializer.new(invoices)
   end
+
+  def update 
+    invoice = Invoice.find(id: params[:id], merchant_id: params[:merchant_id])
+
+    if invoice.nil?
+      return render json: { error: "Invoice not found" }, status: :not_found
+    end
+
+    if params[:coupon_id].present?
+      coupon = Coupon.find(id: params[:coupon_id], merchant_id: params[:merchant_id])
+
+      if coupon.nil?
+        return render json: { error: "Coupon not found or doesn't belong to this merchant" }, status: :not_found
+      end
+
+      invoice.update(coupon: coupon)
+    end
+
+    render json: InvoiceSerializer.new(invoice), status: :ok
+  end
 end
