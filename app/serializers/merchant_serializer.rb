804cbb749ad4b5c68d1d7553f77542dcd24a80c2
Beverly Green <greenbev000@gmail.com>
2025-03-11 14:56:41 -0500

    Issue with adding new merchant on front end, had to add these attributes to merchant serializer

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,6 +1,15 @@
 class MerchantSerializer
   include JSONAPI::Serializer
-  attributes :name, :coupons_count, :invoice_coupon_count
+  attributes :name
+  # :coupons_count, :invoice_coupon_count
+
+  attribute :coupons_count do |merchant|
+    merchant.try(:coupons)&.count || 0
+  end
+
+  attribute :invoice_coupon_count do |merchant|
+    merchant.try(:invoices)&.where.not(coupon_id: nil)&.count || 0
+  end
   
   attribute :item_count, if: Proc.new { |merchant, params|
     params && params[:count] == "true" }
